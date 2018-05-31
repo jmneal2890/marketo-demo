9dc0117ac4ed039da4caf89b59436b0030d7a3a7
J Michael Neal <jmneal.audio@gmail.com>
2018-05-31 13:53:29 -0700

    updated readmer

--- public/components/dbh.inc.php
+++ public/components/dbh.inc.php
@@ -2,7 +2,7 @@
 
 $dbServername = "marketo-demo_mysql_1";
 $dbUsername = "root";
-$dbPassword = "surfpaso";
+$dbPassword = "password";
 $dbName = "marketo_demo";
 
 $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
